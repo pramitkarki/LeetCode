acb210955a30dad678e68ce1c157fbfef0805d00
pramitkarki <44563043+pramitkarki@users.noreply.github.com>
2023-07-29 12:32:45 +0545

    Time: 0 ms (100%), Space: 39.9 MB (77.56%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -5,14 +5,18 @@ class Solution {
         int[][] dp = new int[n][n];
         for(int x[] : dp)
             Arrays.fill(x, -1);
-        return solve(0, n - 1, nums, dp) >= 0;
+        int sum = 0;
+        for(int x : nums) sum += x;
+        int p1 = solve(0, n - 1, nums, dp);
+        return p1 - (sum - p1) >= 0;
     }
     static int solve(int l, int r, int[] nums,int[][] dp){
+        if(l > r) return 0;
         if(l == r) return nums[l];
         if(dp[l][r] != -1) return dp[l][r];
 
-        int left = nums[l] - solve(l + 1, r, nums, dp);
-        int right = nums[r] - solve(l, r - 1, nums, dp);
+        int left = nums[l] + Math.min(solve(l + 2, r, nums, dp), solve(l + 1, r - 1, nums, dp));
+        int right = nums[r] + Math.min(solve(l, r - 2, nums, dp), solve(l + 1, r - 1, nums, dp));
         
         return dp[l][r] = Math.max(left, right);
     }
